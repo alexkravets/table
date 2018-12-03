f9f3431f0a11b511f6df6e57838be0b6321857c9
Alexander Kravets <a@kra.vc>
2018-12-03 17:48:50 +0200

    Add File adapter for component

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 module.exports = {
+  File:                  require('./lib/file'),
   Dynamo:                require('./lib/dynamo'),
   ResourceExistsError:   require('./lib/errors/ResourceExistsError'),
   ResourceNotFoundError: require('./lib/errors/ResourceNotFoundError')
