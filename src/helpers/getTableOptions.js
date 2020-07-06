ee5840540022fc2ee8e637b1b60b72685fc53c28
Oleksandr Kravets <a@kra.vc>
2020-07-06 22:48:34 +0800

    Add default table options

--- src/helpers/getTableOptions.js
+++ src/helpers/getTableOptions.js
@@ -2,8 +2,9 @@
 
 const get = require('lodash.get')
 
-const getTableOptions = (config, tableId) => {
-  const tables = get(config, 'tables', {})
+/* istanbul ignore next */
+const getTableOptions = (config, tableId = 'default') => {
+  const tables = get(config, 'tables', { default: {} })
   const tableOptions = JSON.parse(JSON.stringify(tables[tableId]))
 
   const region  = get(config, 'aws.region', 'local')
